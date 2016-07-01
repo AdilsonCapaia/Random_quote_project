9463f59e975400bc6beced6e2b3601224784ce59
Adilson Capaia <adilsoncapaia@hotmail.com>
2016-07-02 00:01:45 +0200

    add the tags for to the quote object

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ var controlQuote = 0 ;
 // declaration of my arrays of objects
 var myquotes = [
 
-  {quote : "Don't cry because it's over, smile because it happened.", source :" Dr. Seuss ", citation : 'google sites' , year : 1880, tags :"life"},
+  {quote : "Don't cry because it's over, smile because it happened.", source :" Dr. Seuss ", citation : 'google sites' , year : 1880, tags :" Life"},
   {quote : " You only live once, but if you do it right, once is enough. ", source : "Mae West", citation : " google's site " , year : 1980, tags :"Humor"},
-  {quote : " If you have no critics, you'll likely have no success. ", source : "Malcom X", citation : " Brain Quote " , year : 1967, tags :"Sccess, Life"},
-  {quote : " All that I am, or I hope to be. I owe to my angel mother. ", source : "Abraham Lincoln", citation : " Brain Quote " , year : 1880, tags :"Mother's days"}
+  {quote : " If you have no critics, you'll likely have no success. ", source : "Malcom X", citation : " Brain Quote " , year : 1967, tags :" Success-Life"},
+  {quote : " All that I am, or I hope to be. I owe to my angel mother. ", source : "Abraham Lincoln", citation : " Brain Quote " , year : 1880, tags :" Mother's days"}
 ]
 
 // creation and definition of my  "getRandomQuote " function 
@@ -23,58 +23,57 @@ var myquotes = [
 function getRandomQuote(){
 	
 	//generate a random position from 0 to the length of array myquotes
-	var NewPositionQuote = Math.floor(Math.random() *myquotes.length);
+   var NewPositionQuote = Math.floor(Math.random() *myquotes.length);
 	
-	// if controlQuote variable is greater than the length of  array of objects "myquotes" then 
-	// "controlQuote" and "lastQuote" array, both are set to zero, this mean that de quotes 
-	//can be regenerated again. all quotes have been already shown.
+   // if controlQuote variable is greater than the length of  array of objects "myquotes" then 
+   // "controlQuote" and "lastQuote" array, both are set to zero, this mean that de quotes 
+   //can be regenerated again. all quotes have been already shown.
 	
-	if( controlQuote >= myquotes.length ) { 
-		controlQuote = 0; 
-		for( var i=0 ; i<= myquotes.length; i +=1){
-			lastQuote.pop();
-		}
-		console.log(lastQuote);
-	}
+   if( controlQuote >= myquotes.length ) { 
+	  controlQuote = 0; 
+	  for( var i=0 ; i<= myquotes.length; i +=1){
+		lastQuote.pop();
+	  }
+	  console.log(lastQuote);
+   }
 	
-	// loop that control the quotes that have not  been displayed yet. 
-	//but , just if  "controlQuote" is greater than 0.
-	if(controlQuote > 0){
+   // loop that control the quotes that have not  been displayed yet. 
+   //but , just if  "controlQuote" is greater than 0.
+   if(controlQuote > 0){
 		
-		while(lastQuote.indexOf(NewPositionQuote) !== -1){
-			NewPositionQuote = Math.floor(Math.random() *myquotes.length);
-		}
-		lastQuote.push(NewPositionQuote);
-		controlQuote +=1;
-		console.log('quote number '+ NewPositionQuote);
-		return myquotes[NewPositionQuote];
-	} 
+	  while(lastQuote.indexOf(NewPositionQuote) !== -1){
+		NewPositionQuote = Math.floor(Math.random() *myquotes.length);
+	  }
+	  lastQuote.push(NewPositionQuote);
+	  controlQuote +=1;
+	  console.log('quote number '+ NewPositionQuote);
+	  return myquotes[NewPositionQuote];
+   } 
 	
-	controlQuote +=1;
-	lastQuote.push(NewPositionQuote);
-	console.log('first time '+ NewPositionQuote);
-	console.log(lastQuote);
-	return myquotes[NewPositionQuote];
+   controlQuote +=1;
+   lastQuote.push(NewPositionQuote);
+   console.log('first time '+ NewPositionQuote);
+   console.log(lastQuote);
+   return myquotes[NewPositionQuote];
 }
 
 // creation and definition of "random_BG " function 
 // will generate the random color to background
 function random_BG(){
-	document.body.style.backgroundColor = 'rgb( ' + Math.floor(Math.random()*255) + ','+ Math.floor(Math.random() *255) +','+ Math.floor(Math.random()*255)+ ')';
+   document.body.style.backgroundColor = 'rgb( ' + Math.floor(Math.random()*255) + ','+ Math.floor(Math.random() *255) +','+ Math.floor(Math.random()*255)+ ')';
 }
 
 // creation and definition of my  "printQuote " function 
 // "printQuote " will print the selected  content of the quote to the page .
 function printQuote(){
 	
-	
-	objecto = getRandomQuote();
-	var message = '<p class="quote"> ' + objecto['quote']  + '</p> '; 
-	message += '<p class="source"> '+ objecto['source'] + '<span class="citation"> ';
-    message += objecto['citation'] + ' </span> <span class="year"> '+ objecto['year'] + '</span> </p> ';
-	random_BG();// call of the random_BG for change the Background first
-	var div = document.getElementById('quote-box').innerHTML= message;
-	setTimeout('printQuote()', 30000);
+   objecto = getRandomQuote();
+   var message = '<p class="quote"> ' + objecto['quote']  + '</p> '; 
+   message += '<p class="source"> '+ objecto['source'] + '<span class="citation"> ';
+   message += objecto['citation'] + ' </span> <span class="year"> '+ objecto['year'] + '</span>';
+   message += '<span class="tags">'+ objecto['tags']  +'</span> </p> ';
+   random_BG();// call of the random_BG for change the Background first
+   var div = document.getElementById('quote-box').innerHTML= message;
+   setTimeout('printQuote()', 30000);
 }
 
-//printQuote();
