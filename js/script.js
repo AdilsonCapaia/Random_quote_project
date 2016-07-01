4c86ec1af577392fd08b4af5a79a0089f344afd0
Adilson Capaia <adilsoncapaia@hotmail.com>
2016-07-01 04:33:01 +0200

    created 2 util functions
    created 2 functions , randomQuote for generate randomic quote, and
    printQuote for print the content of quote to the main page.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,29 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+// declaration of my arrays of objects
+
+var myquotes = [
+
+  {quote : "Don't cry because it's over, smile because it happened.", source :" Dr. Seuss ", citation : 'google sites' , year : 1880},
+  {quote : " I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best. ", source : " Marilyn Monroe ", citation : " google's site " , year : 1980}
+]
+
+// creation and definition of my  "getRandomQuote " function 
+// getRandomQuote will generate a random quote from "myquotes" array and return a quote object.
+function getRandomQuote(){
+	console.log(myquotes[Math.floor(Math.random() *myquotes.length)] );
+	return myquotes[Math.floor(Math.random() *myquotes.length)] ;
+}
+// creation and definition of my  "printQuote " function 
+// "printQuote " will print the selected  content of the quote to the page .
+function printQuote(){
+	
+	objecto = getRandomQuote()
+	var message = '<p class="quote"> ' + objecto['quote']  + '</p> '; 
+	message += '<p class="source"> '+ objecto['source'] + '<span class="citation"> ';
+    message += objecto['citation'] + ' </span> <span class="year"> '+ objecto['year'] + '</span> </p> ';
+	var div = document.getElementById('quote-box').innerHTML= message;
+}
+
+printQuote();
